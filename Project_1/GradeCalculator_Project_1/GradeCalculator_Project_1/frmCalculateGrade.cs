132ed774fa02c22493a13b0977d4affdd1be765a
hwaymel <42789557+hwaymel@users.noreply.github.com>
2018-09-18 21:25:57 -0600

    Add files via upload

---
+++ Project_1/GradeCalculator_Project_1/GradeCalculator_Project_1/frmCalculateGrade.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeCalculator_Project_1
+{
+    public partial class frmCalculateGrade : Form
+    {
+        public frmCalculateGrade()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Declaring variables for use in if statements
+            decimal numberGrade = Convert.ToDecimal(txtNumberGrade.Text);
+            string letterGrade = "";
+
+            //If statements to determine/set letter grade variable based on numeric value entered
+            if (numberGrade >= 88)
+            {
+                letterGrade = "A";
+            }
+            else if (numberGrade >= 80 && numberGrade <= 87)
+            {
+                letterGrade = "B";
+            }
+            else if (numberGrade >= 68 && numberGrade <=79)
+            {
+                letterGrade = "C";
+            }
+            else if (numberGrade >= 60 && numberGrade <= 67)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+
+            //Displaying letter grade in text box and resetting focus
+            txtLetterGrade.Text = letterGrade;
+            txtNumberGrade.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close form when clicking exit
+            this.Close();
+        }
+    }
+}
